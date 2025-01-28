d9953091f86e238acea04328ba9a36d04a601d25
Maxence Lenoir <maxence.lenoir1206@gmail.com>
2025-01-28 23:15:34 +0100

    Update version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,56 +1,73 @@
-import React, {useState, useRef, useEffect} from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-const Header = ({user}) => {
-  const ref = useRef(null)
-  useOutsideAlerter(ref)
+const Header = ({ user }) => {
+  const ref = useRef(null);
+  useOutsideAlerter(ref);
   const [dropDown, setDropDown] = useState(false);
 
   function useOutsideAlerter(ref) {
     useEffect(() => {
-        function handleClickOutside(event) {
-            if (ref.current && !ref.current.contains(event.target)) {
-              setDropDown(false)
-            }
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setDropDown(false);
         }
-        // Bind the event listener
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            // Unbind the event listener on clean up
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
     }, [ref]);
   }
-  
+
   const renderContent = () => {
-    if(user && Object.keys(user).length === 0) {
-      return null
+    if (user && Object.keys(user).length === 0) {
+      return null;
     }
 
-    switch(user) {
+    switch (user) {
       case false:
-        return <ul><li data-test='login'><a href={`${process.env.REACT_APP_HOSTNAME}/auth/google`}>Se connecter avec Google</a></li></ul>
+        return (
+          <ul>
+            <li data-test="login">
+              <a href={`${process.env.REACT_APP_HOSTNAME}/auth/google`}>
+                Se connecter avec Google
+              </a>
+            </li>
+          </ul>
+        );
       default:
         return (
-            <ul>
-              <li><Link to={`/weekbets`}>Les paris de la semaine</Link></li>
-              <li><Link to='/stats'>Stats</Link></li>
-              <li><Link to='/mesparis'>Mes Paris</Link></li>
-              <li><Link to={`/profile/${user._id}`}>Mon profil</Link></li>
-              <li data-test='logout'><a href={`${process.env.REACT_APP_HOSTNAME}/api/logout`}>Déconnexion</a></li>
-            </ul>
-        )
+          <ul>
+            <li>
+              <Link to={`/weekbets`}>Les paris de la semaine</Link>
+            </li>
+            <li>
+              <Link to="/stats">Stats</Link>
+            </li>
+            <li>
+              <Link to="/mesparis">Mes Paris</Link>
+            </li>
+            <li>
+              <Link to={`/profile/${user._id}`}>Mon profil</Link>
+            </li>
+            <li data-test="logout">
+              <a href={`${process.env.REACT_APP_HOSTNAME}/api/logout`}>
+                Déconnexion
+              </a>
+            </li>
+          </ul>
+        );
     }
-  }
+  };
 
   return (
     <nav ref={ref}>
       <div className="nav-wrapper">
-        <Link 
-          to={user ? `/leagues` : '/'} 
-          className="brand"
-        >
+        <Link to={user ? `/leagues` : "/"} className="brand">
           Loosamax
         </Link>
         <div className="toggle-button" onClick={() => setDropDown(!dropDown)}>
@@ -58,19 +75,18 @@ const Header = ({user}) => {
           <span className="bar"></span>
           <span className="bar"></span>
         </div>
-        <div className={`navbar-links ${dropDown ? 'active' : null}`}>
+        <div className={`navbar-links ${dropDown ? "active" : null}`}>
           {renderContent()}
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-const mapStateToProps = ({user}) => {
-  return { 
-    user
-  }
-} 
+const mapStateToProps = ({ user }) => {
+  return {
+    user,
+  };
+};
 
-
-export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk";
 
 // CSS
-import './scss/index.scss'
+import "./scss/index.scss";
 
 // Internal imports
-import App from './components/App';
-import reducers from './reducers';
+import App from "./components/App";
+import reducers from "./reducers";
 
 // Useful to use the Redux extension debugger
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  reducers, 
-  composeEnhancers(applyMiddleware(thunk))
-)
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
 );
